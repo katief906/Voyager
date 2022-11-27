0a87d25bcc3dd803b397b4c58f5fbe284212cf00
katief906 <katief906@gmail.com>
2022-11-27 17:16:51 -0500

    saving to avoid losing due to iTerm crashing

--- app/controllers/api/v1/stops_controller.rb
+++ app/controllers/api/v1/stops_controller.rb
@@ -5,6 +5,7 @@ class Api::V1::StopsController < ApiController
   end
 
   def new
+    binding.pry
   end
 
   private
--- app/javascript/react/components/NewStopForm.js
+++ app/javascript/react/components/NewStopForm.js
@@ -1,3 +1,4 @@
+import _ from "lodash"
 import React, { useState, useEffect } from "react"
 
 const NewStopForm = (props) => {
@@ -8,39 +9,54 @@ const NewStopForm = (props) => {
 
   const fetchCity = async() => {
     try {
+      debugger
       const cityId = props.match.params.city_id
       const response = await fetch(`/api/v1/cities/${cityId}`)
       if (!response.ok) {
         const errorMessage = `${response.status} (${response.statusText})`
         throw new Error(errorMessage)
       }
-      const cityData = await response.json()
-      setCity(cityData)
+      const responseBody = await response.json()
+      debugger
+      setCity(responseBody.city)
     } catch(error) {
       console.error(`Error in fetch: ${error.message}`)
     }
   }
 
-  // const fetchStops = async() => {
-  //   try {
-  //     const itinerary.id = props.match.params.itinerary_id
-  //     const cityId = city.id
-  //   } catch() {
-
-  //   }
-  // }
+  const fetchStops = async() => {
+    try {
+      debugger
+      const itineraryId = props.match.params.itinerary_id
+      const cityId = city.id
+      const response = await fetch (`/api/v1/itineraries/${itineraryId}/cities/${cityId}/stops/new`)
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`
+        throw new Error(errorMessage)
+      }
+      const responseBody = await response.json()
+      setStops(responseBody)
+    } catch(error) {
+      console.error(`Error in fetch: ${error.message}`)
+    }
+  }
 
   useEffect(() => {
+    debugger
     fetchCity()
   }, [])
 
-  if (readyToFetchStops === true) {
-    fetchStops()
-  }
+  let stopFormHeader
+
+  useEffect(() => {
+    debugger
+    !_.isEmpty(city) && fetchStops()
+  }, [city])
+  
 
   return(
     <div>
-      <h1>Hi from the New Stop Form!</h1>
+      <h1>Add New Stop</h1>
     </div>
   )
 }
