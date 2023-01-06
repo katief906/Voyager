887dd3b96effb12542bb2ddaa3512d89cc2c5ea6
katief906 <katief906@gmail.com>
2023-01-06 16:35:17 -0500

    Added additional states and custom useEffect hooks to CitiesList and CountryShowPage components

--- app/javascript/react/components/CitiesList.js
+++ app/javascript/react/components/CitiesList.js
@@ -4,17 +4,17 @@ import CityTile from "./CityTile"
 
 const CitiesList = (props) => {
   const [cities, setCities] = useState([])
-  const [selectedCity, setSelectedCity] = useState({
-    name: "",
-    geonameid: "",
-    latitude: "",
-    longitude: "",
-    population: ""
-  })
+  const [readyToMakeCityTiles, setReadyToMakeCityTiles] = useState(false)
+  const [loadingMessage, setLoadingMessage] = useState("")
+  const [responseMessage, setResponseMessage] = useState("")
+  const [cityTiles, setCityTiles] = useState([])
+  const [selectedCity, setSelectedCity] = useState({})
   const [foundOrCreatedCity, setFoundOrCreatedCity] = useState({})
   const [shouldRedirect, setShouldRedirect] = useState(false)
 
   const fetchCities = async() => {
+
+    setLoadingMessage("Loading...")
     try {
       const countryId = props.country.id
       const response = await fetch(`/api/v1/countries/${countryId}/cities`)
@@ -24,10 +24,32 @@ const CitiesList = (props) => {
         throw new Error(errorMessage)
       }
       const citiesData = responseBody.cities.sort((a,b) => b.population - a.population)
+      setLoadingMessage("")
       setCities(citiesData)
+      setReadyToMakeCityTiles(true)
     } catch(error) {
       console.error(`Error in fetch: ${error.message}`)
+      setLoadingMessage("")
+      setResponseMessage("No cities found")
     }
+  }
+
+  const makeCityTiles = () => {
+    setResponseMessage(`${cities.length} cities found:`)
+    cities.sort((a, b) => a.name - b.name)
+    const tiles = cities.map((city) => {
+      return(
+        <CityTile
+        key={city.geonameid}
+        city={city}
+        cityId={city.geonameid}
+        country={props.country}
+        selectedCity={selectedCity}
+        setSelectedCity={setSelectedCity}
+        />
+        )
+      })
+    setCityTiles(tiles)
   }
 
   const findOrCreateCity = async() => {
@@ -52,45 +74,28 @@ const CitiesList = (props) => {
       console.error(`Error in fetch: ${error.message}`)
     }
   }
-
-  let message
-  let cityTiles
-  
+      
   useEffect(() => {
     fetchCities()
   }, [])
 
-  if (selectedCity.name !== ""){
-    findOrCreateCity()
-  }
-  
-  if (cities.length) {
-    message = `${cities.length} cities found:`
-    cities.sort((a, b) => a.name - b.name)
-    cityTiles = cities.map((city) => {
-      return(
-        <CityTile
-        key={city.geonameid}
-        city={city}
-        cityId={city.geonameid}
-        country={props.country}
-        selectedCity={selectedCity}
-        setSelectedCity={setSelectedCity}
-        />
-        )
-    })
-  } else {
-    message = "No cities found"
-  }
+  useEffect(() => {
+    readyToMakeCityTiles === true && makeCityTiles()
+  }, [readyToMakeCityTiles])
 
-  if (shouldRedirect) {
-    return <Redirect to={`/cities/${foundOrCreatedCity.id}`} />
+  useEffect(() => {
+    selectedCity.name && findOrCreateCity()
+  }, [selectedCity])
+  
+  if (shouldRedirect === true) {
+    return(<Redirect to={`/cities/${foundOrCreatedCity.id}`} />)
   }
 
   return(
     <div>
       <h4 className = "text-center">
-        {message}
+        {loadingMessage}
+        {responseMessage}
       </h4>
       {cityTiles}
     </div>
